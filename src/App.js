b18aa5417a649ff4ab3af4e5483f74fb0f3f3b9e
juminzoomout <juminzoomout@gmail.com>
2024-08-02 20:58:35 +0900

    add to merge

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Add from './routes/Add';
 import Edit from './routes/Edit';
+import HomePage from './routes/HomePage';
+import DbAdd from './routes/DbAdd';
+import DbSearch from './routes/DbSearch';
+import NutritionStatsPage from './routes/NutritionStatsPage';
+import ConsumptionStatsPage from './routes/ConsumptionStatsPage';
+import WasteStatsPage from './routes/WasteStatsPage';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
