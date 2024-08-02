8d4157c2336ad2e58d69e9152760d442ce6f5e4d
juminzoomout <juminzoomout@gmail.com>
2024-08-02 20:30:43 +0900

    add addpage and related components

---
+++ src/components/CalendarComponent.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import '../../node_modules/react-calendar/dist/Calendar.css';
+import moment from 'moment';
+import styled from 'styled-components';
+
+const CalendarContainer = styled.div`
+  .react-calendar {
+    border: none;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  .react-calendar__tile {
+    height: 50px;
+  }
+
+  .react-calendar__tile--active {
+    background: #1e90ff;
+    color: white;
+  }
+`;
+
+function CalendarComponent() {
+  const [selDate, setSelDate] = useState(new Date());
+
+  const onDateChange = (date) => {
+    setSelDate(date);
+  };
+
+  const showDate = moment(selDate).format('YYYY-MM-DD');
+
+  return (
+    <CalendarContainer>
+      <Calendar onChange={onDateChange} name="date" value={selDate}></Calendar>
+      <h3>{showDate}</h3>
+    </CalendarContainer>
+  );
+}
+
+export default CalendarComponent;
