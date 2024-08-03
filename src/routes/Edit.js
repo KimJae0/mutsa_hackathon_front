bd9e30c9e467e54d06ef194c01dbd13a7111523a
juminzoomout <juminzoomout@gmail.com>
2024-08-04 05:12:06 +0900

    add NewMoney, complete Add, Edit, refactor InputBox, install package

--- src/routes/Edit.js
+++ src/routes/Edit.js
@@ -1,5 +1,47 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import CalendarComponent from '../components/CalendarComponent';
+import InputBox from '../components/InputBox';
+import Dropdown from '../components/Dropdown';
+import { Button } from '../components/ButtonGroup';
+import NewMoney from '../components/NewMoney';
+
 function Edit() {
-  return <div></div>;
+  const [moneyList, setMoneyList] = useState([]);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('데이터 저장');
+    navigate('/');
+  };
+
+  const addNewMoney = () => {
+    console.log('Adding new money');
+    setMoneyList([...moneyList, moneyList.length + 1]);
+  };
+
+  return (
+    <div>
+      <h1>기록 추가하기</h1>
+      <form onSubmit={handleSubmit}>
+        <NewMoney />
+        {moneyList.map((index) => (
+          <NewMoney key={index} index={index} />
+        ))}
+        <button type="button" onClick={() => addNewMoney()}>
+          새로운 소비 추가
+        </button>
+        <Button type="button" variant="link" onClick={() => navigate('/')}>
+          취소
+        </Button>
+        <button type="button" onClick={() => console.log('삭제')}>
+          삭제하기
+        </button>
+        <input type="submit" value="저장"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Edit;
