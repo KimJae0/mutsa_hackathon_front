657928671552462d02831a3d9c0553fd812f1c5d
jiyooniboon <jiyooniboon@g.skku.edu>
2024-08-04 16:17:47 +0900

    env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,15 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import React from "react";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnKuzMTIo5FxwaHicMqXHABKosx4jFUm8",
-  authDomain: "ssuik-wallet.firebaseapp.com",
-  projectId: "ssuik-wallet",
-  storageBucket: "ssuik-wallet.appspot.com",
-  messagingSenderId: "765847562735",
-  appId: "1:765847562735:web:f0c6a6f3a36c222f936e1a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
